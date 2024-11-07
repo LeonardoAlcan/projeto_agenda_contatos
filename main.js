ef1ae55e67d5a1aad53bb55028fb5ef3b8cd96eb
Leonardo <leonardoalcan11@gmail.com>
2024-11-07 16:21:22 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,56 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji triste" />';
+const atividades = [];
+const notas = [];
+const totalContatos = document.getElementById('total-contatos');
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    atualizaMediaFinal();
+});
+
+
+function adicionaLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    if (notas.includes(inputNotaAtividade.value) || atividades.includes(inputNomeAtividade.value)) {
+        alert(`O numero: ${inputNotaAtividade.value} ou contato: ${inputNomeAtividade.value} já foi inserido`)
+    } else {
+        atividades.push(inputNomeAtividade.value);
+        notas.push(inputNotaAtividade.value);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal();
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = notas.length;
+
+    totalContatos.innerHTML = somaDasNotas;
+}
